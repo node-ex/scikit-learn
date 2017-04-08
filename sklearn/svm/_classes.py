9b2db11443cc1b447c8b11191b157d1a02889a25
Ivan Nazarov <ivannnnz@gmail.com>
2017-04-09 02:27:26 +0300

    Update to address #8711

--- sklearn/svm/_classes.py
+++ sklearn/svm/_classes.py
@@ -1506,6 +1506,26 @@ class SVDD(BaseLibSVM):
         dec = self._decision_function(X)
         return dec
 
+    def predict(self, X):
+        """
+        Perform classification on samples in X.
+
+        For an one-class model, +1 or -1 is returned.
+
+        Parameters
+        ----------
+        X : {array-like, sparse matrix}, shape (n_samples, n_features)
+            For kernel="precomputed", the expected shape of X is
+            [n_samples_test, n_samples_train]
+
+        Returns
+        -------
+        y_pred : array, shape (n_samples,)
+            Class labels for samples in X.
+        """
+        y = super(SVDD, self).predict(X)
+        return np.asarray(y, dtype=np.intp)
+
     @deprecated(
         "The probA_ attribute is deprecated in version 0.23 and will be "
         "removed in version 0.25.")
